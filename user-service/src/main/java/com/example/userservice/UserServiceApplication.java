5b183bba47ce018a59b61868b8e1e9a6b3d516da
js.min <js.min@hansol.com>
2023-01-23 18:29:32 +0900

    RestTemplate,FeignClient 적용

--- user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -1,21 +1,27 @@
 package com.example.userservice;
 
+import com.example.userservice.error.FeignErrorDecoder;
 import com.example.userservice.service.Testinf;
 import com.example.userservice.service.UserService;
 import com.example.userservice.service.UserServiceimpl2;
 import com.netflix.discovery.converters.Auto;
+import feign.Logger;
 import org.h2.engine.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class UserServiceApplication {
 
 
@@ -28,10 +34,23 @@ public class UserServiceApplication {
     public BCryptPasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
-    /*
+
     @Bean
-    AuthenticationManager authenticationManager(AuthenticationConfiguration auth ) throws  Exception{
-        return auth.getAuthenticationManager();
-    }*/
+    @LoadBalanced
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
+    @Bean
+    public Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
+
+/*
+    @Bean
+    public FeignErrorDecoder getFeignErrorDecoder(){
+        return new FeignErrorDecoder();
+    }
+*/
 
 }
--- user-service/src/main/java/com/example/userservice/service/UserServiceimpl.java
+++ user-service/src/main/java/com/example/userservice/service/UserServiceimpl.java
@@ -1,22 +1,31 @@
 package com.example.userservice.service;
 
 
+import com.example.userservice.client.OrderServiceClient;
 import com.example.userservice.dto.UserDto;
 import com.example.userservice.exception.UserNotFoundException;
 import com.example.userservice.jpa.UserEntity;
 import com.example.userservice.jpa.UserRepository;
 import com.example.userservice.vo.ResponseOrder;
+import feign.FeignException;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.modelmapper.spi.MatchingStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,17 +33,27 @@ import java.util.UUID;
 
 @Service
 @Primary
+@Slf4j
 public class UserServiceimpl implements UserService{
 
 
     UserRepository userRepository;
     BCryptPasswordEncoder passwordEncoder;
+    Environment env;
+    RestTemplate restTemplate;
+    OrderServiceClient orderServiceClient;
 
     @Autowired
-    public UserServiceimpl(@Lazy UserRepository userRepository,@Lazy BCryptPasswordEncoder passwordEncoder ){
+    public UserServiceimpl(UserRepository userRepository,
+                           BCryptPasswordEncoder passwordEncoder,
+                           Environment env,
+                           RestTemplate restTemplate,
+                           OrderServiceClient orderServiceClient) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
-
+        this.env = env;
+        this.restTemplate = restTemplate;
+        this.orderServiceClient = orderServiceClient;
     }
     @Override
     public UserDto createUser(UserDto userDto) {
@@ -61,8 +80,31 @@ public class UserServiceimpl implements UserService{
 
         UserDto returnUserDto = new ModelMapper().map(userEntity, UserDto.class);
 
-        List<ResponseOrder> orders = new ArrayList<>();
-        returnUserDto.setOrders(orders);
+   //     List<ResponseOrder> orders = new ArrayList<>();
+        /* Using as rest template
+        String orderUrl = String.format(env.getProperty("order_service.url"), userId);
+        ResponseEntity<List<ResponseOrder>> orderListResponse =
+                restTemplate.exchange(orderUrl, HttpMethod.GET, null,
+                        new ParameterizedTypeReference<List<ResponseOrder>>() {
+        });
+        List<ResponseOrder> orderlist = orderListResponse.getBody();
+        */
+
+        /* Using a feign client */
+        /* Feign exception handling */
+/*        List<ResponseOrder> orderlist = null;
+        try{
+            orderlist = orderServiceClient.getOrdersByUserId(userId);
+        } catch (FeignException ex){
+            log.error(ex.getMessage());
+        }*/
+
+        /* ErrorDecoder */
+        List<ResponseOrder> orderlist = orderServiceClient.getOrdersByUserId(userId);
+
+
+        returnUserDto.setOrders(orderlist);
+
         return returnUserDto;
     }
 
